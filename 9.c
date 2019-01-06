c4c9e9f10228a8af717f433ea6ce6c79376a4a8c
Anush Kumar V <anush070@gmail.com>
2019-01-06 19:05:58 +0530

    modified 9

--- 9.c
+++ 9.c
@@ -3,8 +3,19 @@
 
 void main() {
     int i;
-	#pragma omp parallel for num_threads(8)
+	#pragma omp parallel for num_threads(8) 
 	for(i = 97; i<=122; i++)
 	    printf("%c",i);
     printf("\n");
 }
+/* If they ask u to print it in order use the below code
+void main() {
+    int i;
+	#pragma omp parallel for num_threads(8) ordered
+	for(i = 97; i<=122; i++)
+	#pragma omp ordered
+	    printf("%c",i);
+    printf("\n");
+}
+*/
+
